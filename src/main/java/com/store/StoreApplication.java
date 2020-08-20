8b62e85644fd0bc927f405df13b2e40def9a6e47
mwilc <mwil99@interia.pl>
2020-08-20 22:45:22 +0200

    database try

--- src/main/java/com/store/StoreApplication.java
+++ src/main/java/com/store/StoreApplication.java
@@ -14,18 +14,12 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 @SpringBootApplication
 //@EntityScan(basePackages = {"model"})
 //@ComponentScan(basePackageClasses = {repository.MagazineRepository.class})
-public class StoreApplication implements CommandLineRunner {
+public class StoreApplication  {
 
     public static void main(String[] args) {
         SpringApplication.run(StoreApplication.class, args);
     }
 
-    @Autowired
-    MagazineRepository magazineRepository;
-    @Override
-    public void run(String... args) throws Exception {
-        this.magazineRepository.save(new Magazine("Fruit"));
-        this.magazineRepository.save(new Magazine("jbhda"));
-        this.magazineRepository.save(new Magazine("bsdfis"));
+
     }
-}
+
